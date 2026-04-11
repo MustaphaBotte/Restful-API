fa143137ebb1f53ee6dd875cc8c081084c3fe145
mostapha <mostaphabotte@gmail.com>
2026-04-11 14:10:49 +0100

    more about win32 api

---
+++ Speech/Speech/Program.cs
@@ -0,0 +1,19 @@
+﻿using SpeechLib;
+namespace Speech
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SpVoice voice = new SpVoice();
+            voice.Rate=1;
+            voice.Volume = 80;
+            voice.Rate = 2;
+            voice.Priority = SpeechVoicePriority.SVPAlert;
+
+            string text = "hello every one";
+            voice.Speak(text);
+
+        }
+    }
+}
